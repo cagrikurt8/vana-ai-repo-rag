b79965a8ac7d355f08ef38eea01f0816be00ecd1
Çağrı Kurt <cagri.kurt8@outlook.com>
2025-04-11 14:57:40 +0300

    vanna knowledge base moved to a tool

--- main.py
+++ main.py
@@ -1,41 +1,65 @@
 from fastapi import FastAPI
-from langgraph.graph import START, END, StateGraph
-from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+from langgraph.graph import START, END, StateGraph, MessagesState
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage, ToolMessage
 from langchain_core.runnables import RunnableConfig
 from langchain_core.documents import Document
+from langchain_core.tools import tool
 from langgraph.checkpoint.memory import MemorySaver
+from langgraph.prebuilt import tools_condition, ToolNode
 from langchain_openai import AzureChatOpenAI, AzureOpenAIEmbeddings
 from langchain import hub
 from langchain_chroma import Chroma
 from pydantic import BaseModel
-from typing import TypedDict, List
+from typing import List
 
 
 class Question(BaseModel):
     question: str
 
 
-class State(TypedDict):
+class State(MessagesState):
     question: str
     answer: str
     context: List[Document]
 
 
+@tool
+def search_vanna_knowledge_base(question: str):
+    """Search the Vanna knowledge base for relevant information. Vanna AI is a SQL Generator project. Knowledge base contains information about the project, tools, and source code.
+    This function uses the Chroma database to perform a similarity search based on the question provided.
+    Args:
+        question (str): The question to search for.
+    Returns:
+        List[Document]: A list of relevant documents from the knowledge base.
+    """
+    # Perform a similarity search in the Chroma database
+    embedding = AzureOpenAIEmbeddings(
+        api_version="2024-10-21",
+        azure_deployment="text-embedding-3-small-1"
+    )
+    chroma_db_path = "./chroma_db"
+    chroma = Chroma(collection_name="test-task-collection", embedding_function=embedding, persist_directory=chroma_db_path)
+    context = chroma.similarity_search(question, k=5)
+
+    return context
+
+
 class QAAgent:
-    def __init__(self, llm: AzureChatOpenAI, embedding: AzureOpenAIEmbeddings, chroma_db_path: str):
+    def __init__(self, llm: AzureChatOpenAI):
         self.llm = llm
-        self.embedding = embedding
-        self.chroma = Chroma(collection_name="test-task-collection", embedding_function=self.embedding, persist_directory=chroma_db_path)
+        self.tools = [search_vanna_knowledge_base]
+        self.llm_with_tools = self.llm.bind_tools(self.tools)
         self.prompt = hub.pull("rlm/rag-prompt")
+        self.sys_msg = open("./prompts/system_message.txt", "r").read()
     
+    
+    def call_model(self, state: State):
+        #context = self.chroma.similarity_search(question, k=5)
+        #messages = self.prompt.invoke({"question": question, "context": context})
+        messages = [SystemMessage(content=self.sys_msg)] + state.get("messages")
+        response = self.llm_with_tools.invoke(messages)
 
-    def call_model(self, state: State, config: RunnableConfig):
-        question = state.get("question")
-        context = self.chroma.similarity_search(question, k=5)
-        messages = self.prompt.invoke({"question": question, "context": context})
-        response = self.llm.invoke(messages, config)
-
-        return {"question": question, "answer": response.content, "context": context}
+        return {"messages": response}
 
 
     def build_graph(self, checkpointer: MemorySaver = None):
@@ -43,10 +67,11 @@ class QAAgent:
 
         # Define nodes
         builder.add_node("assistant", self.call_model)
-
+        builder.add_node("tools", ToolNode(self.tools))
         # Define edges
         builder.add_edge(START, "assistant")
-        builder.add_edge("assistant", END)
+        builder.add_conditional_edges("assistant", tools_condition)
+        builder.add_edge("tools", "assistant")
 
         graph = builder.compile(checkpointer=checkpointer)
 
@@ -60,6 +85,7 @@ app = FastAPI()
 def read_root():
     return {"Message": "Welcome to test-task!"}
 
+
 @app.post("/ask/")
 async def ask(question: Question):
     # Initialize the LLM and embedding model
@@ -67,19 +93,22 @@ async def ask(question: Question):
         api_version="2024-10-21",
         azure_deployment="gpt-4o"
     )
-    embedding = AzureOpenAIEmbeddings(
-        api_version="2024-10-21",
-        azure_deployment="text-embedding-3-small-1"
-    )
-    chroma_db_path = "./chroma_db"
-    agent = QAAgent(llm, embedding, chroma_db_path)
+    agent = QAAgent(llm)
     # Initialize the graph
     graph = agent.build_graph()
-    response = graph.invoke(
-        {
-            "question": question.question
-        },
-        config={"configurable": {"thread_id": "1"}}
-    )
+    response = graph.invoke({"messages": [HumanMessage(content=question.question)]})
+    question = question.question
+    answer = response.get("messages")[-1].content
+    response_metadata = response.get("messages")[-1].response_metadata
 
-    return response
+    if len(response.get("messages")) > 2 and isinstance(response.get("messages")[2], ToolMessage):
+        context = response.get("messages")[2].content
+    else:
+        context = "Out-of-scope question."
+
+    return {
+        "question": question,
+        "answer": answer,
+        "context": context,
+        "response_metadata": response_metadata
+    }
